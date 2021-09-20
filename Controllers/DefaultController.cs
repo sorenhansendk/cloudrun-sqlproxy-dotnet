4b492cc8014a5a26c2f9cfd844f260a1f97cb0fe
Søren Hansen <soren@dinero.dk>
2021-09-20 21:55:14 +0200

    Initial commit.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CloudRunDemo.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DefaultController : ControllerBase
+    {
+        public DefaultController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome!");
+        }
+    }
+}
